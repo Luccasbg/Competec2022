c79c33005743acf8846572d35261f843553ec715
Luccas Guimarães <contato.luccasguimaraes@gmail.com>
2022-10-28 19:45:23 -0300

    Finalização da simulação

--- POO/simulacaoJogo/Main.java
+++ POO/simulacaoJogo/Main.java
@@ -8,23 +8,34 @@ public class Main {
       Arqueiro arqueiro = new Arqueiro(10,50);
       Guerreiro guerreiro = new Guerreiro(20, 30);
 
-      System.out.println("Insira a força do arco desejado:");
+      System.out.println("Arqueiro - Insira a força do arco desejado:");
       int forcaArco = sc.nextInt();
 
-      System.out.println("Insira a força da espada desejada:");
+      System.out.println("Guerreiro - Insira a força da espada desejada:");
       int forcaEspada = sc.nextInt();
-
 
       arqueiro.setForcaArco(forcaArco);
       guerreiro.setForcaEspada(forcaEspada);
 
-      while(arqueiro.getVida() > 0 && guerreiro.getVida() > 0){
-          arqueiro.atacarInimigo(guerreiro);
-          if(guerreiro.getVida() <= 0){
-              System.out.println("O personagem morreu!");
-          }else{
-              System.out.println("Vida do guerreiro: " + guerreiro.getVida());
+      System.out.println("Quem irá atacar?");
+      String personagemAtacante = sc.next();
+
+      if(personagemAtacante.toLowerCase().equals("arqueiro")){
+          while(guerreiro.getVida() > 0){
+              arqueiro.atacarInimigo(guerreiro);
+              if(guerreiro.getVida() <= 0){
+                  System.out.println("O personagem morreu!");
+              }
           }
+      }else if(personagemAtacante.toLowerCase().equals("guerreiro")){
+          while(arqueiro.getVida() > 0){
+              guerreiro.atacarInimigo(arqueiro);
+              if(arqueiro.getVida() <= 0){
+                  System.out.println("O personagem morreu!");
+              }
+          }
+      }else{
+          System.out.println("Erro! Insira um personagem válido!");
       }
     }
 }
